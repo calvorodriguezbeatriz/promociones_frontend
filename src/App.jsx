7b80efd96bac1e7f8667b15eff6eb35a8d6c3419
Beatriz <calvorodriguezbeatriz@gmail.com>
2022-08-29 14:00:35 +0200

    autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import  PromotionView from './components/promotions/PromotionView';
 
+ 
 const App=()=> {
+
   return (
     <div className="App">
-      <Login/>
+
       <Router>
         <Routes>
+          <Route path="/login" element={<Login/>}/>
           <Route path="/" element={<PromotionsList />}/>
           <Route path="promotion/:id" element={<PromotionView />}/>
         </Routes>
